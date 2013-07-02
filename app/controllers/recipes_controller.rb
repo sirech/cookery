e5d8746b6c8a8b7d53bf76d8b28937c44ca2cf77
Mario Fernandez <mario.fernandez@hceris.com>
2013-07-02 23:49:07 +0200

    Remove old pagination hard coded limit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,7 @@ class RecipesController < ApplicationController
 
   # GET /recipes
   def index
-    @recipes = Recipe.order(:name).page(params[:page]).per(5)
+    @recipes = Recipe.order(:name).page(params[:page])
   end
 
   # GET /recipes/1
