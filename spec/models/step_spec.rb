03fa2538a895910493d7e92c67aa5011ecce5442
Mario Fernandez <mario.fernandez@amiando.com>
2013-07-02 09:38:54 +0200

    Moar tests

--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -3,8 +3,12 @@ require 'spec_helper'
 describe Step do
 
   describe '#duration' do
-    it 'should not accept negative durations' do
+    it 'should not accept negative numbers' do
       expect { Step.create! name: 'Invalid', duration: -100 }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'should accept zero' do
+      expect { Step.create! name: 'Valid', duration: 0 }.not_to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 end
