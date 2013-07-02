bea3e2495b88b4d1effa2fb4f9fdac427543274e
Mario Fernandez <mario.fernandez@hceris.com>
2013-07-02 19:19:29 +0200

    FIX: paperclip urls

--- config/initializers/paperclip_defaults.rb
+++ config/initializers/paperclip_defaults.rb
@@ -1,4 +1,4 @@
 Paperclip::Attachment.default_options.update({
-  :path => ':rails_root/public/system/:class/:attachment/:hash/:style.:extension',
-  :hash_secret => 'jVx+n3XsBh1S0nOu1CAwcsZvjxdGO4hFQHTqCGr+a8m09UHpVQ/lU7GXevsTvbX8U/DGW8T1T4RFMpH2X9lKfXix8GRnRj2O1qsBGp4YYvJmjCTJLAlYUhI4Nt3oIinzfg04vwbGYg1ajwXrnIpnNu7ZAnYxI03veAhhJ4bPvFg='
+    url: '/system/:class/:attachment/:hash/:style.:extension',
+    hash_secret: 'jVx+n3XsBh1S0nOu1CAwcsZvjxdGO4hFQHTqCGr+a8m09UHpVQ/lU7GXevsTvbX8U/DGW8T1T4RFMpH2X9lKfXix8GRnRj2O1qsBGp4YYvJmjCTJLAlYUhI4Nt3oIinzfg04vwbGYg1ajwXrnIpnNu7ZAnYxI03veAhhJ4bPvFg='
 })
