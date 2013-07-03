12074c1077c98240b3f199ceabcd1d205d0052c6
Mario Fernandez <mario.fernandez@hceris.com>
2013-07-03 23:38:52 +0200

    Patch paperclip so that validation erros are passed in a form that can be understood by simple form

--- config/initializers/paperclip_defaults.rb
+++ config/initializers/paperclip_defaults.rb
@@ -1,3 +1,5 @@
+require 'paperclip_extensions'
+
 Paperclip::Attachment.default_options.update({
     url: '/system/:class/:attachment/:hash/:style.:extension',
     hash_secret: 'jVx+n3XsBh1S0nOu1CAwcsZvjxdGO4hFQHTqCGr+a8m09UHpVQ/lU7GXevsTvbX8U/DGW8T1T4RFMpH2X9lKfXix8GRnRj2O1qsBGp4YYvJmjCTJLAlYUhI4Nt3oIinzfg04vwbGYg1ajwXrnIpnNu7ZAnYxI03veAhhJ4bPvFg='
