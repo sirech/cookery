9639a2751a0c99378f3dc90d4d3c216cafef45ce
Mario Fernandez <mario.fernandez@hceris.com>
2013-07-03 23:39:36 +0200

    Add new input type to generate preview of pic when editing

---
+++ app/inputs/picture_input.rb
@@ -0,0 +1,12 @@
+class PictureInput < SimpleForm::Inputs::FileInput
+  def input
+    out = '' # the output string we're going to build
+    # check if there's an uploaded file (eg: edit mode or form not saved)
+    if object.send("#{attribute_name}?")
+      # append preview image to output
+      out << template.image_tag(object.send(attribute_name).url(:thumb), class: 'thumbnail', id: 'picture')
+    end
+    # append file input. it will work accordingly with your simple_form wrappers
+    (out << @builder.file_field(attribute_name, input_html_options)).html_safe
+  end
+end
