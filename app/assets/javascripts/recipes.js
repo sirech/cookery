bb8162f97e0e68a59b9910bdc2a2b2d73771ce3c
Mario Fernandez <mario.fernandez@hceris.com>
2013-07-07 23:44:49 +0200

    First version of the step modal popup

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,6 +1,62 @@
 (function() {
 
+  var stepCreator = function(modal, send, form) {
+
+    var success = function(data) {
+      // $(this).each(function(){
+      //   this.reset();
+      // });
+      $(modal)
+        .trigger('step:created', data)
+        .modal('hide');
+    };
+
+    var error = function(data) {
+      console.log(data);
+    };
+
+    return {
+      observe: function() {
+        $(send).click(function() {
+          $(form).submit();
+        });
+
+        $(form)
+          .bind('ajax:complete', function(e, xhr) {
+            if(xhr.status == 0) {
+              success(xhr.responseJSON);
+            } else {
+              error(xhr.responseJSON);
+            }
+          });
+      }
+    };
+  };
+
+  var stepUpdater = function(modal, steps_container) {
+
+    field = $(steps_container).find('input');
+    steps = [];
+    ids = []
+
+    return {
+      observe: function() {
+        $(modal)
+          .on('step:created', function(e, data) {
+            steps.push(data);
+            $(steps_container).find('.accordion')
+              .loadTemplate($('#step-template'), steps);
+            ids.push(data.id)
+            field.val(ids);
+          });
+      }
+    };
+  };
+
   $(function() {
     $('*[rel=tooltip]').tooltip();
+
+    stepCreator('#add-step', '#add-step-confirm', '#add-step-form').observe();
+    stepUpdater('#add-step', '#steps').observe();
   });
 })();
