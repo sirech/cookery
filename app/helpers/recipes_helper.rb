8403e50709d63248874180bf52b21ffee17ef454
Mario Fernandez <mario.fernandez@hceris.com>
2013-07-10 23:48:53 +0200

    Basic show layout

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,12 +1,17 @@
 module RecipesHelper
-  def difficulty_tag difficulty
+  def picture_tag(recipe, size=:medium)
+    image_tag recipe.picture.url(size), class: 'img-rounded'
+  end
+
+  def difficulty_tag(difficulty)
     to_label = {
       'easy' => 'success',
       'medium' => 'warning',
       'difficult' => 'important'
     }
     content_tag(:div,
-      content_tag(:span, difficulty, class: "pull-right label label-#{to_label[difficulty]}"),
+      content_tag(:span, difficulty,
+        class: "pull-right label label-#{to_label[difficulty]}"),
       class: 'span6')
   end
 end
