4c54c609a6c32dc2de5f02bf97e254902ac6823f
Mario Fernandez <mario.fernandez@hceris.com>
2013-08-15 16:54:23 +0200

    Add start/stop for deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,8 @@ require "bundler/capistrano"
 set :application, 'cookery'
 set :repository,  'https://github.com/sirech/cookery.git'
 
+set :thin_config, "/etc/thin.d/#{application}.yml"
+
 set :scm, :git
 
 set :user, 'sirech'
@@ -26,5 +28,11 @@ namespace :deploy do
   desc 'Symlinks the database.yml'
   task :symlink_db, roles: :app do
     run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+
+  [:start, :stop].each do |t|
+    task t do
+      run "cd #{current_path} && bundle exec thin #{t} -C #{thin_config}"
+    end
   end
 end
